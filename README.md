42eb50baa0dac2ec23bd40c0502fdfcc0e5166ca
Timothy Chen Allen <timothychenallen@gmail.com>
2015-07-28 16:41:01 -0400

    How to set fill colors for each facet?

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ggplot_facets
+
+## How do I set a separate fill color for each facet?
+
+This fake data set shows output from two factories.  I want to create a 
+`ggplot2` image with two facets, one for each factory.  Currently,
+the image looks like this, with just one fill color for both facets:
+
+![Factory A vs. Factory B](factories.png)
+
+Instead of having just one color for both facets, I want to set the
+fill color for each facet separately:
+
+* red for Factory A
+* blue for Factory B
+
+How do I do that?
---
+++ facet_fill_colors.R
@@ -0,0 +1,18 @@
+# 1. Load libraries
+require(ggplot2)
+
+# 3. Create a plot with two facets
+yr <- c(2013,2014,2015,2013,2014,2015)
+widgets.made <- c(10,20,40,15,30,60)
+factory <- c(rep(" Factory A",3),rep("Factory B",3))
+df <- data.frame(factory, widgets.made, yr)
+png("./factories.png")
+# Note the geom_bar "fill" parameter-- I want this
+# to be different for each facet; 
+# Red for Factory A
+# Blue for Factory B
+ggplot(df) + 
+        aes(yr, widgets.made) + 
+        geom_bar(stat="identity", fill="green") +
+        facet_grid(. ~ factory)
+dev.off()
